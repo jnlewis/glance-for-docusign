c29daec7f1f9f632dd399a09e87f83f1a000f370
Jeffrey Lewis <novensiles@gmail.com>
2025-01-23 17:57:31 +0800

    Cleanup unused codes

--- config/config.ts
+++ config/config.ts
@@ -14,7 +14,7 @@ export interface AppConfig {
 export const Config: AppConfig = {
   environment: process.env.NEXT_PUBLIC_ENVIRONMENT || 'dev',
   apiHost: process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3010',
-  aiApiKey: 'mOK2p74t45G2gTYm5kLRvv2q9f9Xp',
+  aiApiKey: process.env.NEXT_PUBLIC_AI_API_KEY || '',
   aiApiGenerate: 'https://gaxvwyokzf.execute-api.us-east-1.amazonaws.com/generate-text', // us-east-1
   docusignClientId: process.env.DOCUSIGN_CLIENT_ID || '',
   docusignClientSecret: process.env.DOCUSIGN_CLIENT_SECRET || '',
